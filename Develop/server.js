7182456b855256b6849612dee3d58880c47430b1
Brian Adams <bpeteradams@gmail.com>
2021-04-20 12:19:03 -0400

    basic set up of serverA

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require('./routes/html-routes')(app);
+require('./routes/api-routes')(app);
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening on PORT: ${PORT}`);
+})
