bd878dc39608bc79ef122f9f47f0dbd8206363a2
Brian Adams <bpeteradams@gmail.com>
2021-04-20 13:01:50 -0400

    set up api get and post routes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,4 +3,9 @@ const db = require('../db/db.json');
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res)=>res.json(db));
+
+    app.post('/api/notes', (req, res)=>{
+        db.push(req.body)
+        
+        return res.json(req.body)})
 }
