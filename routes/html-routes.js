f408846ffa22fbed50b84f6b253040ad1d094dcd
Brian Adams <bpeteradams@gmail.com>
2021-04-24 15:28:10 -0400

    fixed routing issue with index.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,13 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = (app) =>{
-    app.get('/', (req,res)=>{
-        res.sendFile(path.join(__dirname, '../public/index.html'))
-    })
+    
     
     app.get('/notes', (req, res) =>{
         res.sendFile(path.join(__dirname, '../public/notes.html'))
+    })
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     })
 }
